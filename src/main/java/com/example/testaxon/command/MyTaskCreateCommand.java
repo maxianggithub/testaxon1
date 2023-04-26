f3309e44bd8abf9b067d1a3423125b4b36915506
Gerard Klijs <gerard.klijs@axoniq.io>
2023-04-26 13:45:33 +0200

    fix

--- src/main/java/com/example/testaxon/command/MyTaskCreateCommand.java
+++ src/main/java/com/example/testaxon/command/MyTaskCreateCommand.java
@@ -2,7 +2,7 @@ package com.example.testaxon.command;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.axonframework.commandhandling.model.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateIdentifier;
 
 @Data
 @AllArgsConstructor
--- src/main/java/com/example/testaxon/controller/MyTaskController.java
+++ src/main/java/com/example/testaxon/controller/MyTaskController.java
@@ -4,7 +4,6 @@ package com.example.testaxon.controller;
 import com.example.testaxon.command.MyTaskCreateCommand;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.common.IdentifierFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyTaskController {
     private final IdentifierFactory identifierFactory = IdentifierFactory.getInstance();
+    private final CommandGateway commandGateway;
 
-    @Autowired
-    private CommandGateway commandGateway;
+    public MyTaskController(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
 
     @RequestMapping(value = "/test/anox/", method = RequestMethod.POST)
     public void createMyTask(@RequestParam String title, @RequestParam String describe) {
